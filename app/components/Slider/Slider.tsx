25542bd91df2e87decee7386cdb436479c5ee72c
michele <michle.carmagnani@gmail.com>
2023-02-21 15:13:11 +0100

    feat: tailwind

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -3,8 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
-// import Carousel from 'react-bootstrap/Carousel';
-import { Carousel } from 'flowbite-react';
+import { useRouter } from 'next/navigation';
 
 import { CarInfo } from 'typings';
 import CardItem from '../SliderItem/SliderItem';
@@ -14,35 +13,88 @@ type Props = {
   data: CarInfo[][];
   data2: CarInfo[];
 };
+let count = 0;
 
 const Slider = ({ data, data2 }: Props) => {
-  const refControl = useRef<any>(null);
-  const [index, setIndex] = useState(0);
+  const router = useRouter();
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+  // const handleOnNextClicked = () => {
+  //   count = (currentIndex + 1) % data2.length;
+  //   setCurrentIndex(count);
+  //   router.push(`/#item${count}`);
+  // };
+  // const handleOnPrevClicked = () => {
+  //   const carsArrayLength = data2.length;
+  //   count = (currentIndex + carsArrayLength - 1) % carsArrayLength;
+  //   setCurrentIndex(count);
+  // };
+  // const handleAutoPlay = () => {
+  //   const interval = setInterval(() => {
+  //     handleOnNextClicked();
+  //   }, 3000);
+  // };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     setCurrentIndex(window.scrollX);
+  //   };
+
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
+  console.log(currentIndex);
   return (
-    <>
-      <div className="carousel carousel-start max-w-sm p-4 space-x-2 bg-none rounded-box">
-        {data2.map((carItem) => (
-          <div key={carItem.id} className="carousel-item ">
+    <div className="relative">
+      <div
+        // onScroll={handleOnNextClicked}
+        className="carousel carousel-start p-4 space-x-4 max-w-sm"
+      >
+        {data2.map((carItem, index) => (
+          <div id={`item${index}`} key={carItem.id} className="carousel-item ">
             <Image
-              id={`item${index}`}
               width={800}
               height={600}
               src={carItem.imageUrl}
               className=""
               alt={carItem.modelName}
             />
+            {/* <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 carousel-item">
+              <a
+                href={`#item${index - 1}`}
+                className="btn btn-circle"
+                onClick={() => setCurrentIndex(index)}
+              >
+                ❮
+              </a>
+              <a
+                href={`#item${index + 1}`}
+                className="btn btn-circle"
+                onClick={() => setCurrentIndex(index)}
+              >
+                ❯
+              </a>
+            </div> */}
           </div>
         ))}
       </div>
       <div className="flex justify-center w-full py-2 gap-2">
         {data2.map((carItem, index) => (
-          <Link key={carItem.id} href={`#item${index}`} className="btn btn-xs">
-            {index + 1}
-          </Link>
+          <a
+            key={carItem.id}
+            href={`#item${index}`}
+            className={`${
+              currentIndex === index
+                ? 'bg-black h-3 w-3 rounded-lg'
+                : 'h-3 w-3 bg-slate-400/20 rounded-lg'
+            } `}
+            onClick={() => setCurrentIndex(index)}
+          ></a>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
